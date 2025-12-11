15678b1a46913ab1689d8bb68828318c449814c7
SimoneFortino <simonefortino41@gmail.com>
2025-12-11 11:25:20 +0100

    rebuild iniziale

--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,11 @@ using System.Windows.Forms;
 
 namespace GateControl
 {
-  public partial class Form1 : Form
-  {
-    public Form1()
+    public partial class Form1 : Form
     {
-      InitializeComponent();
+        public Form1()
+        {
+            InitializeComponent();
+        }
     }
-  }
 }
