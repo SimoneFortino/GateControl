24975bc2a7fdcad5cc1181da08d85c613c2df71f
SimoneFortino <simonefortino41@gmail.com>
2025-12-11 11:37:28 +0100

    commit1

--- GateControlForm.Designer.cs
+++ GateControlForm.Designer.cs
@@ -1,6 +1,6 @@
 namespace GateControl
 {
-    partial class Form1
+    partial class GateControlForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,10 +29,16 @@ namespace GateControl
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // GateControlForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "GateControlForm";
+            this.Text = "Automatic Gate Control";
+            this.ResumeLayout(false);
         }
 
         #endregion
---
+++ GateControlForm.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace GateControl
+{
+    public partial class GateControlForm : Form
+    {
+        public GateControlForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
